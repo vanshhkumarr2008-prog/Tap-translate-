08e6c50ba108a3fc4561346f8d391a16cf5670a4
vanshhkumarr2008-prog <vanshhkumarr2008@gmail.com>
2026-02-26 19:14:07 +0530

    Implement overlay permission check in MainActivity
    Added overlay permission check in MainActivity.

--- app/src/main/java/com/tap/translate/MainActivity.kt
+++ app/src/main/java/com/tap/translate/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.tap.translate
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Abhi humne layout set nahi kiya hai, isliye crash ho sakta hai
-        // Ek simple empty view set karte hain
-        setContentView(android.view.View(this))
+        
+        // Overlay Permission Check
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName"))
+            startActivityForResult(intent, 123)
+            Toast.makeText(this, "Please allow 'Display over other apps' to use 🌟 icon", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Permission already granted! Add the 🌟 icon from notification panel", Toast.LENGTH_LONG).show()
+            finish() 
+        }
     }
 }
