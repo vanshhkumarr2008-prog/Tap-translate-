94543c1a0508929a8a6e8e162135e681a81b0b6a
vanshhkumarr2008-prog <vanshhkumarr2008@gmail.com>
2026-02-26 19:54:51 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tap/translate/MainActivity.kt
+++ app/src/main/java/com/tap/translate/MainActivity.kt
@@ -4,22 +4,25 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // Overlay Permission Check
+        setContentView(R.layout.activity_main) // Design dikhane ke liye line
+
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+        btnSettings.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:$packageName"))
+            startActivity(intent)
+        }
+
         if (!Settings.canDrawOverlays(this)) {
             val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:$packageName"))
             startActivityForResult(intent, 123)
-            Toast.makeText(this, "Please allow 'Display over other apps' to use 🌟 icon", Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(this, "Permission already granted! Add the 🌟 icon from notification panel", Toast.LENGTH_LONG).show()
-            finish() 
         }
     }
 }
