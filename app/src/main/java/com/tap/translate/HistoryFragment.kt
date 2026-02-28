54ffe737a93e0074563c91cd80f44c6f880212bf
vanshhkumarr2008-prog <vanshhkumarr2008@gmail.com>
2026-02-28 12:31:56 +0530

    Update HistoryFragment.kt

--- app/src/main/java/com/tap/translate/HistoryFragment.kt
+++ app/src/main/java/com/tap/translate/HistoryFragment.kt
@@ -5,11 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HistoryFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // fragment_history.xml ko load karega
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.historyList)
+        
+        val dbHelper = DatabaseHelper(requireContext())
+        val historyData = mutableListOf<Pair<String, String>>()
+        
+        // Database se data nikalna
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("SELECT original, translated FROM history ORDER BY id DESC", null)
+        
+        if (cursor.moveToFirst()) {
+            do {
+                historyData.add(Pair(cursor.getString(0), cursor.getString(1)))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = HistoryAdapter(historyData)
+        
+        return view
     }
 }
-
