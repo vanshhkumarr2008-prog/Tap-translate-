5edfce247631332c13431f141926cc85ddb02535
vanshhkumarr2008-prog <vanshhkumarr2008@gmail.com>
2026-03-02 21:26:35 +0530

    Create ProActivity.kt

---
+++ app/src/main/res/layout/app/src/main/java/com/vansh/taptranslatepro/app/src/main/res/drawable/app/src/main/res/drawable/res/layout/app/src/main/java/com/vansh/taptranslatepro/ProActivity.kt
@@ -0,0 +1,12 @@
+package com.vansh.taptranslatepro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pro)
+    }
+}
